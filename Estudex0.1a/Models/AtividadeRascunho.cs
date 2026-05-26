6984be0f7e15d2c0217d13b8d2f6ebd9482984f0
msgsant <miguelssantanatbl@gmail.com>
2026-05-26 20:32:27 -0300

    Inserir questoes e opcoes

---
+++ Estudex0.1a/Models/AtividadeRascunho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudex0._1a.Models
+{
+    public class AtividadeRascunho
+    {
+        public string Titulo { get; set; }
+        public int PontuacaoMaxima { get; set; }
+        public Utilizador Orientador { get; set; }
+        public NivelDificuldade NivelDificuldade { get; set; }
+        public List<AtividadePergunta> Perguntas { get; set; } = new();
+    }
+}
